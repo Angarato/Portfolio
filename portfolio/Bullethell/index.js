54906f4a10600f553c0e3e77fd97820d374865fd
Angarato <mvl86@msn.com>
2026-01-08 16:01:03 +0100

    goeie shit

---
+++ portfolio/Bullethell/index.js
@@ -0,0 +1,76 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d');
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+class Player {
+    constructor(x, y, radius, color) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+}
+
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        c.fillStyle = this.color
+        c.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, 'red')
+player.draw()
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach(projectile => {
+        projectile.update();
+    });
+
+}
+
+window.addEventListener('click', (event) => {
+
+    projectiles.push(new Projectile(
+        canvas.width / 2,
+        canvas.height / 2,
+        5,
+        'blue',
+        {
+            x: 1, y: 1
+        })
+    )
+})
+
+animate()
---
+++ portfolio/brocodelessons/index.js
@@ -0,0 +1,20 @@
+
+
+// let age = 12;
+// let message = age >= 18 ? "you're an adult" : "you're a minor";
+
+// console.log(message)
+
+// let time = 16;
+// let greeding = time < 12 ? "good morning" : "good afternoon!"
+// console.log(greeding)
+
+// let isStudent = true;
+// let message = isStudent ? "you are a student" : "you're not a student!"
+// console.log(message);
+
+let phoneNumber = "06-18646076"
+
+phoneNumber = phoneNumber.padEnd(35, "0")
+
+console.log(phoneNumber)
