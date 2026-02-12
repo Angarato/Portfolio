8b3aaf3cc2d2c388d7f63bc5efc2a9a0c037d51c
Angarato <mvl86@msn.com>
2026-02-12 15:26:13 +0100

    12-2

--- portfolio/brocodelessons/index.js
+++ portfolio/brocodelessons/index.js
@@ -1,15 +1,62 @@
-//error
 
-try{
-const dividend = window.prompt("Enter a dividend: ");
-const divisor = window.prompt("Enter a divisor: ");
 
-const result = dividend / divisor;
 
-console.leg(result);
+
+function walkDog() {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const dogWalked = true;
+
+            if (dogWalked) {
+                resolve("thou walketh teh dawg");
+            }
+            else {
+                reject("thou did not walketh teh dawg")
+            }
+        }, 1500)
+    });
 }
-catch(error){
-    console.error(error);
+
+function cleanKitchen() {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const kitchenCleaned = false;
+
+            if (kitchenCleaned) {
+                resolve("thou cleaneth teh kitchen");
+            }
+            else {
+                reject("thou did not cleaneth teh kitchen")
+            }
+        }, 2500)
+    });
+
 }
 
-console.log("the end")
+function takeTrash() {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+
+            const trashTakenOut = true;
+
+            if(trashTakenOut){
+            resolve("thou tooketh out teh trash");
+            }
+            else{
+                reject("thou did not tooketh out teh trash")
+            }
+        }, 500)
+    })
+
+}
+
+walkDog().then(value => { console.log(value); return cleanKitchen() })
+    .then(value => { console.log(value); return takeTrash() })
+    .then(value => { console.log(value); console.log("thou finished thine tasks") })
+    .catch(error => console.error(error));
+    
