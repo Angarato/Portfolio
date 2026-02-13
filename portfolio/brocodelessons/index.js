dbba60f46a2ef59fe7d3dd0953a4a0c8d57f7b80
Angarato <mvl86@msn.com>
2026-02-13 16:00:15 +0100

    derp

--- portfolio/brocodelessons/index.js
+++ portfolio/brocodelessons/index.js
@@ -1,62 +1,141 @@
+//weatherapp
 
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card")
+const apiKey = "4ea18e3431b3682f51893c7c663c38eb";
 
+weatherForm.addEventListener("submit", async event => {
 
+    event.preventDefault();
 
-function walkDog() {
+    const city = cityInput.value;
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
+    if(city){
+        try{
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        }
+        catch(error){
+            console.error(error);
+            displayError(error);
+        }
+    }
+    else{
+        displayError("Please enter a City")
+    }
 
-            const dogWalked = true;
+})
 
-            if (dogWalked) {
-                resolve("thou walketh teh dawg");
-            }
-            else {
-                reject("thou did not walketh teh dawg")
-            }
-        }, 1500)
-    });
+async function getWeatherData(city){
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    if(!response.ok){
+        throw new Error("could not fetch weather data")
+    }
+
+    return await response.json();
 }
 
-function cleanKitchen() {
+function displayWeatherInfo(data){
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
+    const {name: city, 
+           main: {temp, humidity}, 
+           weather: [{description, id}]} = data;
+    
+    card.textContent = "";
+    card.style.display = "flex";
 
-            const kitchenCleaned = false;
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const tempFDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
 
-            if (kitchenCleaned) {
-                resolve("thou cleaneth teh kitchen");
-            }
-            else {
-                reject("thou did not cleaneth teh kitchen")
-            }
-        }, 2500)
-    });
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${(temp - 273.15).toFixed(2)}°C`;
+    tempFDisplay.textContent = `(${((temp - 273.15) * (9/5) + 32).toFixed(2)}°F)`;
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = `${description}`;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    tempFDisplay.classList.add("tempFDisplay")
+    humidityDisplay.classList.add("humidityDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(tempFDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+}
+
+function getWeatherEmoji(weatherID){
+
+    switch(true){
+        case (weatherID >= 200 && weatherID < 300):
+            return "⛈️"; 
+        case (weatherID >= 300 && weatherID < 400):
+            return "🌧️";    
+        case (weatherID >= 500 && weatherID < 600):
+            return "🌧️"; 
+        case (weatherID >= 600 && weatherID < 700):
+            return "🌨️"; 
+        case (weatherID >= 700 && weatherID < 800):
+            return "🌁"; 
+        case (weatherID === 800):
+            return "☀️"; 
+        case (weatherID >= 800 && weatherID < 810):
+            return "🌥️"; 
+        default: 
+            return "?";
+    }
 
 }
 
-function takeTrash() {
+function displayError(message){
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-
-            const trashTakenOut = true;
-
-            if(trashTakenOut){
-            resolve("thou tooketh out teh trash");
-            }
-            else{
-                reject("thou did not tooketh out teh trash")
-            }
-        }, 500)
-    })
-
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
 }
 
-walkDog().then(value => { console.log(value); return cleanKitchen() })
-    .then(value => { console.log(value); return takeTrash() })
-    .then(value => { console.log(value); console.log("thou finished thine tasks") })
-    .catch(error => console.error(error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(response => {
+
+//         if(!response.ok){
+//             throw new Error("couldnt fetch resource")
+//         }
+//         return response.json();
+//     })
+    
+    
+//     .then(data => console.log(data.name ))
+    
+//     .catch(error => console.error(error));
     
